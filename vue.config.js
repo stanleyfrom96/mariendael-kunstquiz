ffb284d1394420fe6e35ae73f25560ee5aa7a64c
stanley <stanleyfrom96@gmail.com>
2023-09-20 11:03:15 +0200

    update icon and title

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
+      args[0].title = 'Mariëndael Kunstquiz';
       args[0].meta = {
         viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
       };
